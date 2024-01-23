ec7e5066829dc8be1f3844fc6f4e2dceb09c91be
Nadddy12 <abd.nad12@gmail.com>
2024-01-23 18:37:09 +0100

    ajouter les liens

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,19 +7,25 @@ const items = [
         id:1,
         title:"Agence Immobilier",
         img:"/immobilier.png",
-        desc:"Un exemple simple de site web immobilier avec une fonctionnalité de recherche et une base de données. Vous pouvez créer un compte et ajouter votre maison. Réalisé avec React, serveur Node.js, MongoDB et Firebase (MERN)."
+        desc:"Un exemple simple de site web immobilier avec une fonctionnalité de recherche et une base de données. Vous pouvez créer un compte et ajouter votre maison. Réalisé avec React, serveur Node.js, MongoDB et Firebase (MERN). PS: (Pour lancer le site web, cela pourrait prendre 5 minutes car il est hébergé gratuitement.)",
+        github:"https://github.com/Nadddy12/habitaitMERN",
+        demo:"https://fakhri-immobilier.onrender.com/"
     },
     {
         id:2,
         title:"Wiki et Blog de jeu Elden Ring",
         img:"/elden.png",
-        desc:"Un projet de fin d'études comprenant une section administrateur avec 6 fonctionnalités CRUD pour ajouter des blogs, des guides, des équipements et des sorts, ainsi qu'une section permettant aux utilisateurs normaux d'ajouter et de tester les états de leurs personnages (simulateur de statistiques). Les administrateurs peuvent promouvoir des personnes pour devenir des modérateurs. Réalisé avec React, serveur Node.js, MongoDB"
+        desc:"Un projet de fin d'études comprenant une section administrateur avec 6 fonctionnalités CRUD pour ajouter des blogs, des guides, des équipements et des sorts, ainsi qu'une section permettant aux utilisateurs normaux d'ajouter et de tester les états de leurs personnages (simulateur de statistiques). Les administrateurs peuvent promouvoir des personnes pour devenir des modérateurs. Réalisé avec React, serveur Node.js, MongoDB",
+        github:"https://github.com/Nadddy12/Elden-ring-wiki-personal-project-",
+        demo:"none"
     },
     {
         id:3,
         title:"Projets en construction",
         img:"/next.jpg",
-        desc:"Bientôt, d'autres projets seront disponibles. Utilisation de C#, PhP et JS"
+        desc:"Bientôt, d'autres projets seront disponibles. Utilisation de C#, PhP et JS",
+        github:"none",
+        demo:"none"
     },
 ];
 
@@ -43,7 +49,20 @@ const Single = ({item}) => {
                     <motion.div className="textContainer" style={{y:y}}>
                         <h3>{item.title}</h3>
                         <p>{item.desc}</p>
-                        <button>See Demo</button>
+                        {item.demo !== "none" ? (
+                            <motion.a href={item.demo} whileHover={{scale:1.05}} whileTap={{scale:0.9}} className="demo">
+                                Démo
+                            </motion.a>
+                        ) : (
+                            <span className="disabled-link">Démo</span>
+                        )}
+                        {item.github !== "none" ? (
+                            <motion.a href={item.github} whileHover={{scale:1.05}} whileTap={{scale:0.9}} className="github">
+                                Code sur github
+                            </motion.a>
+                        ) : (
+                            <span className="disabled-link">Code sur github</span>
+                        )}
                     </motion.div>
                 </div>
             </div>
